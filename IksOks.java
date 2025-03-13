2e8511a94c4883853cbd7b0b956da9147b6bf719
Armin Džanić <armin.dzanic@fet.ba>
2025-03-13 16:24:02 +0100

    Add files via upload

---
+++ IksOks.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+public class IksOks
+{
+  public static void main(String[] args)
+  {
+    int[][] Board = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    Scanner scanner = new Scanner(System.in);
+    int CurrentPlayer = 1;
+    PrintBoard(Board);
+
+    while(true)
+    {
+      System.out.print("Player " + CurrentPlayer + " enter a cell number: ");
+      int cell = scanner.nextInt();
+      if(cell < 1 && cell > 9)
+      {
+        System.out.println("You picked the wrong number fool!");
+        continue;
+      }
+      int row = (cell - 1) / 3;
+      int col = (cell - 1) % 3;
+      if(Board[row][col] != 0)
+      {
+        System.out.println("Cell already occupied. Try again.");
+        continue;
+      }
+      Board[row][col] = CurrentPlayer;
+      if(CheckForWin(Board, CurrentPlayer))
+      {
+        System.out.println("Player " + CurrentPlayer + " has won!");
+        PrintBoard(Board);
+        break;
+      }
+      PrintBoard(Board);
+      if(CurrentPlayer == 1)
+      {
+        CurrentPlayer = 2;
+      }
+      else
+      {
+        CurrentPlayer = 1;
+      }
+    }
+  }
+
+  public static void PrintBoard(int[][] Board)
+  {
+    for(int i = 0; i < 3; ++i)
+    {
+      for(int j = 0; j < 3; ++j)
+      {
+        System.out.print(Board[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static boolean CheckForWin(int[][] Board, int CurrentPlayer)
+  {
+    // Prvi red
+    if(Board[0][0] == CurrentPlayer && Board[0][1] == CurrentPlayer && Board[0][2] == CurrentPlayer)
+    {
+      return true;
+    }
+    // Drugi red
+    if(Board[1][0] == CurrentPlayer && Board[1][1] == CurrentPlayer && Board[1][2] == CurrentPlayer)
+    {
+      return true;
+    }
+    // Treci red
+    if(Board[2][0] == CurrentPlayer && Board[2][1] == CurrentPlayer && Board[2][2] == CurrentPlayer)
+    {
+      return true;
+    }
+    // Prva kolona
+    if(Board[0][0] == CurrentPlayer && Board[1][0] == CurrentPlayer && Board[2][0] == CurrentPlayer)
+    {
+      return true;
+    }
+    // Druga kolona
+    if(Board[0][1] == CurrentPlayer && Board[1][1] == CurrentPlayer && Board[2][1] == CurrentPlayer)
+    {
+      return true;
+    }
+    // Treca kolona
+    if(Board[0][2] == CurrentPlayer && Board[1][2] == CurrentPlayer && Board[2][2] == CurrentPlayer)
+    {
+      return true;
+    }
+    // Glavna dijagonala
+    if(Board[0][0] == CurrentPlayer && Board[1][1] == CurrentPlayer && Board[2][2] == CurrentPlayer)
+    {
+      return true;
+    }
+    // Sporedna dijagonala
+    if(Board[0][2] == CurrentPlayer && Board[1][1] == CurrentPlayer && Board[2][0] == CurrentPlayer)
+    {
+      return true;
+    }
+    return false;
+  }
+}
